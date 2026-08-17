a17b6711668033131d161f066d47e1eb92bdde97
Heer Patel <heerpatel132007@gmail.com>
2026-08-17 21:29:53 +0530

    Add files via upload

---
+++ practical-3/problem1.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+using namespace std;
+
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+       
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+}
+
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int marks[] = {75, 45, 90, 60, 30};
+    int n = 5;
+
+   
+    int bubble[5], selection[5], insertion[5];
+
+    for (int i = 0; i < n; i++)
+    {
+        bubble[i] = marks[i];
+        selection[i] = marks[i];
+        insertion[i] = marks[i];
+    }
+
+    bubbleSort(bubble, n);
+    selectionSort(selection, n);
+    insertionSort(insertion, n);
+
+    cout << "Bubble Sort: ";
+    printArray(bubble, n);
+
+    cout << "Selection Sort: ";
+    printArray(selection, n);
+
+    cout << "Insertion Sort: ";
+    printArray(insertion, n);
+
+    return 0;
+}
